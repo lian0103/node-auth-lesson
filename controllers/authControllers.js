5a385f33a6ec357ea1272a0db06d6c2de541b7b6
lien0103 <TP2003007@wistronits.com>
2020-08-26 11:52:17 +0800

    add err type

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -18,13 +18,22 @@ const handleError = (err) => {
             console.log(properties);
             errors[properties.path] = properties.message;
         })
+    }
 
+    //error message
+    if(err.message === "incorrect email"){
+        errors.email = "incorrect email";
+    }
+
+    if(err.message === "incorrect password"){
+        errors.password = "incorrect password";
     }
 
     return errors;
 }
 
 const maxAge = 3 * 24 * 60 * 60;
+
 const createToken = (id) => {
     return jwt.sign({ id }, 'sign up secret str' , {
         expiresIn : maxAge 
@@ -46,7 +55,7 @@ module.exports.signup_post = async (req, res) => {
     try {
         let user = await User.create({ email, password });
         const token = createToken(user._id);
-        res.cookie('jwt',token, { httpOnly:true ,maxAge:maxAge * 1000})
+        res.cookie('jwt',token, { maxAge:maxAge * 1000})
         res.status(201).json(user._id);
     } catch (err) {
         let errors = handleError(err);
@@ -57,16 +66,15 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
-
     try{
         const user = await User.login(email,password);
+        const token = createToken(user._id);
+        res.cookie('jwt',token, { maxAge:maxAge * 1000})
         res.status(200).json({user:user.id});
     }catch(err){
-        console.log(err);
-        res.status(400).json({
-            email:'',
-            password:''
-        });
+        let errors = handleError(err);
+        console.log(errors);
+        res.status(400).json({errors});
     }
 
 }
