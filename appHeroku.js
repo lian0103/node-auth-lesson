94fa54e3e4ee2cef00846293c309cf06c097d339
Jason <gt07891@greattree.com.tw>
2021-09-17 14:28:41 +0800

    use express app

--- appHeroku.js
+++ appHeroku.js
@@ -1,17 +1,14 @@
-const http = require('http');
+const express = require("express");
 
-//設定server網址，因為在本機端測試，所以輸入127.0.0.1
-//const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
+const app = express();
 
-//port 號會由 Heroku 給予，因此不再自行指定
-const port = process.env.PORT || 3006;
+const port = 3008;
+app.set("view engine", "ejs");
 
-//新增一個server並指定他的頁面資訊，內容為'Hello World!'
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!????\n');
+app.use(express.static("public"));
+app.use(express.json());
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(port,()=>{
+    console.log("listen on"+port);
 });
-
-//監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Listening on ${port}`));
