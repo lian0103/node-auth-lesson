8214a41370d30ddd48c68131bd9801d9b94ae62e
Jason <gt07891@greattree.com.tw>
2021-09-17 14:35:38 +0800

    fix port

--- appHeroku.js
+++ appHeroku.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = 3008;
+const port = process.env.PORT || 3006;
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
